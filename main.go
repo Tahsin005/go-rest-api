86035bf813ff60ae08116bef252ee7466d4c5cad
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-05-07 12:48:04 +0600

    Initialize Go module and basic server setup

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "log"
+    "net/http"
+
+    "github.com/gorilla/mux"
+)
+
+func main() {
+    r := mux.NewRouter()
+    http.Handle("/", r)
+
+    log.Println("Server starting on :8080...")
+    if err := http.ListenAndServe(":8080", nil); err != nil {
+        log.Fatal(err)
+    }
+}
