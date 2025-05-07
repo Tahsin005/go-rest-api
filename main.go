ebb572d99e2bb028db0229d020fbf65e120ab15c
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-05-07 13:16:34 +0600

    Add GET / endpoint returning Welcome message

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "encoding/json"
     "log"
     "net/http"
 
@@ -9,6 +10,13 @@ import (
 
 func main() {
     r := mux.NewRouter()
+
+    // Welcome endpoint
+    r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Content-Type", "application/json")
+        json.NewEncoder(w).Encode(map[string]string{"message": "Welcome"})
+    }).Methods("GET")
+
     http.Handle("/", r)
 
     log.Println("Server starting on :8080...")
